a4f05e9ab0b3888852ea13356425d0b65c924d52
Laban Eilers <laban@eilers.com>
2019-07-01 10:25:43 -0400

    Fix powershell

--- selfiecert.cmd
+++ selfiecert.cmd
@@ -1,3 +1,3 @@
 @echo off 
 SET SCRIPT_DIR=%~dp0
-"C:\Program Files\Git\bin\sh.exe" --login -i -- "%SCRIPT_DIR%selfiecert" %*
+"C:\Program Files\Git\bin\sh.exe" --login -i -- "%SCRIPT_DIR%selfiecert.sh" %*
